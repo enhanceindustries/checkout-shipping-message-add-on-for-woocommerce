aa243c42f5621d55fd5cde21d9ab6a08cf94aca3
william dutton <will.dutt@enhanceindustries.com.au>
2019-12-04 18:42:11 +1000

    Initial Release
    Co-authored-by: William Dutton <will.dutt@enhanceindustries.com.au>
    Co-authored-by: Jaren Janio <jaron@enhanceindustries.com.au>

---
+++ README.md
@@ -0,0 +1,23 @@
+## WooCommerce Checkout Shipping Message Add-on
+
+The add-on will allow you to add a custom message to the woocommerce-shipping-totals shipping section of your checkout.
+
+The message will be wrapped by id shipping_message and class woocommerce-shipping-shipping-message.
+
+
+Tags:  australia,  cart, checkout, shipping, shipping message
+
+Requires at least: WooCommerce 3.6.0
+
+Tested up to: 
+* WooCommerce 3.8.1
+* Wordpress 5.3
+
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+## Changelog
+
+- 1.0
+  Initial release
+
---
+++ woocomerce-checkout-shipping-message.php
@@ -0,0 +1,67 @@
+<?php
+/**
+ * Plugin Name: WooCommerce Checkout Shipping Message Add-on
+ * Plugin URI: https://www.enhanceindustries.com.au/
+ * Description: Add notes on Shipping section for custom text
+ * Has been tested on WooCommerce 3.8.1
+ * Author: Enhance Industries
+ * Author URI: https://www.enhanceindustries.com.au/
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+    exit; // Exit if accessed directly
+}
+
+/** Styles **/
+function wpse_load_plugin_css() {
+    $plugin_url = plugin_dir_url( __FILE__ );
+
+    wp_enqueue_style( 'shipping-note-css', $plugin_url . 'assets/css/shipping-note.css' );
+}
+add_action( 'wp_enqueue_scripts', 'wpse_load_plugin_css' );
+
+/**
+*  Override cart-shipping on woocommerce to insert the custom shipping note
+**/
+function ei_locate_template( $template, $template_name, $template_path ) {
+	$basename = basename( $template );
+	if( $basename == 'cart-shipping.php' && is_checkout() ) {
+		$template = trailingslashit( plugin_dir_path( __FILE__ ) ) . 'templates/cart-shipping.php';
+	}
+	return $template;
+}
+add_filter( 'woocommerce_locate_template', 'ei_locate_template', 10, 3 );
+
+function add_shipping_notice( $settings_tab ) {
+	$settings_tab['shipping_notes'] = __( 'Shipping Note' );
+	return $settings_tab;
+}
+add_filter( 'woocommerce_get_sections_shipping', 'add_shipping_notice', 10, 2 );
+
+function shipnote_get_settings( $settings, $current_section ) {
+	$custom_settings = array();
+	if( 'shipping_notes' == $current_section ) {
+		$custom_settings =  array(
+			array(
+			'name' => __( 'Shipping Note' ),
+			'type' => 'title',
+			'desc' => __( 'Add Shipping notes on checkout' ),
+			'id'   => 'ei_shipping_note_lbl'
+			),
+
+			array(
+				'name' => __( 'Message' ),
+				'type' => 'textarea',
+				'desc' => __( 'Message to display on the notice'),
+				'desc_tip' => true,
+				'id'	=> 'msg_threshold'
+			),
+
+			array( 'type' => 'sectionend', 'id' => 'ei_shipping_note_lbl' )
+		);
+		return $custom_settings;
+	} else {
+		return $settings;
+	}
+}
+add_filter( 'woocommerce_get_settings_shipping' , 'shipnote_get_settings' , 10, 2 );
